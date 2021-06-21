81fcd884a1ddbcd39ef7ef9a940929efa2eba37e
Sid Hayoun Lee <sidhlee@gmail.com>
2021-06-21 16:18:34 -0400

    chore: test api key

--- src/App.js
+++ src/App.js
@@ -2,21 +2,13 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const apiKey = process.env.REACT_APP_WEATHER_STACK_API;
+  console.log('hello');
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Weather App</h1>
+        <p>{apiKey}</p>
       </header>
     </div>
   );
