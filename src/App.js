a4071d405ca91b2136f171c64300ae9fcf497241
Zach Bines <zachbines@gmail.com>
2021-06-22 17:27:53 -0400

    created form functionality

--- src/App.js
+++ src/App.js
@@ -43,28 +43,73 @@ const responseObject = {
 
 function App() {
   const apiKey = process.env.REACT_APP_WEATHER_STACK_API;
-  const [weather, setWeather] = useState({});
+  const mapApiKey = process.env.REACT_APP_MAPQUEST_API;
 
-  useEffect(() => {
-    const getWeather = async () => {
-      const response = await fetch(
-        `http://api.weatherstack.com/current?access_key=${apiKey}&query=NewYork`
-      );
-      const parsedResponse = await response.json();
-      console.log(response);
-      console.log(parsedResponse);
-    };
-    // console.log(responseObject);
+  const [weather, setWeather] = useState({});
+  const [map, setMap] = useState("");
+  const [addressInput, setAddressInput] = useState("");
+
+  const getLocation = async (lat, lng) => {
+    const response = await fetch(
+      `https://open.mapquestapi.com/staticmap/v5/map?key=${mapApiKey}&center=${lat},${lng}`
+    );
+    console.log(response);
+    setMap(response);
+  };
+
+  const getCoordinates = async () => {
+    const response = await fetch(
+      `http://www.mapquestapi.com/geocoding/v1/address?key=${mapApiKey}&location=1600+Pennsylvania+Ave+NW,Washington,DC,20500`
+    );
+    const coordinateData = await response.json();
+    return coordinateData.results[0].locations[0].latLng;
+  };
+
+  // weaterAPI call
+  const getWeather = async (lat, lng) => {
+    const response = await fetch(
+      `http://api.weatherstack.com/current?access_key=${apiKey}&query=${lat},${lng}`
+    );
+    const parsedResponse = await response.json();
+    console.log(response);
+    console.log(parsedResponse);
     // setWeather(responseObject);
-    getWeather();
-  }, []);
+  };
+
+  // console.log(responseObject);
+
+  const handleChange = (event) => {
+    console.log(event.target.value);
+    setAddressInput(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const { lat, lng } = getCoordinates(addressInput);
+    getWeather(lat, lng);
+    getLocation();
+  };
+
   return (
     <div className="App">
       <header className="App-header">
         <h1>Weather App</h1>
+        <form onSubmit={handleSubmit}>
+          <label htmlFor="address">Address</label>
+          <input
+            type="text"
+            value={addressInput}
+            id="address"
+            onChange={handleChange}
+          />
+          <button type="submit">Submit</button>
+        </form>
+      </header>
+      <main>
         <p>{apiKey}</p>
         <p>{weather?.current?.weather_descriptions}</p>
-      </header>
+        <img src={map.url} alt={`map of ${weather?.location?.region}`} />
+      </main>
     </div>
   );
 }
