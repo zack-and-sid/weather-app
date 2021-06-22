68c985b936fdf1598687a31c7de600f1e1132241
Sid Hayoun Lee <sidhlee@gmail.com>
2021-06-22 18:37:20 -0400

    feat: render weather info and map from user input

--- src/App.js
+++ src/App.js
@@ -1,35 +1,35 @@
-import { useEffect, useState } from "react";
-import "./App.css";
+import { useEffect, useState } from 'react';
+import './App.css';
 
 const responseObject = {
   request: {
-    type: "City",
-    query: "New York, United States of America",
-    language: "en",
-    unit: "m",
+    type: 'City',
+    query: 'New York, United States of America',
+    language: 'en',
+    unit: 'm',
   },
   location: {
-    name: "New York",
-    country: "United States of America",
-    region: "New York",
-    lat: "40.714",
-    lon: "-74.006",
-    timezone_id: "America/New_York",
-    localtime: "2021-06-21 16:34",
+    name: 'New York',
+    country: 'United States of America',
+    region: 'New York',
+    lat: '40.714',
+    lon: '-74.006',
+    timezone_id: 'America/New_York',
+    localtime: '2021-06-21 16:34',
     localtime_epoch: 1624293240,
-    utc_offset: "-4.0",
+    utc_offset: '-4.0',
   },
   current: {
-    observation_time: "08:34 PM",
+    observation_time: '08:34 PM',
     temperature: 29,
     weather_code: 116,
     weather_icons: [
-      "https://assets.weatherstack.com/images/wsymbols01_png_64/wsymbol_0002_sunny_intervals.png",
+      'https://assets.weatherstack.com/images/wsymbols01_png_64/wsymbol_0002_sunny_intervals.png',
     ],
-    weather_descriptions: ["Partly cloudy"],
+    weather_descriptions: ['Partly cloudy'],
     wind_speed: 0,
     wind_degree: 182,
-    wind_dir: "S",
+    wind_dir: 'S',
     pressure: 1004,
     precip: 0,
     humidity: 65,
@@ -37,7 +37,7 @@ const responseObject = {
     feelslike: 32,
     uv_index: 6,
     visibility: 16,
-    is_day: "yes",
+    is_day: 'yes',
   },
 };
 
@@ -46,48 +46,57 @@ function App() {
   const mapApiKey = process.env.REACT_APP_MAPQUEST_API;
 
   const [weather, setWeather] = useState({});
-  const [map, setMap] = useState("");
-  const [addressInput, setAddressInput] = useState("");
+  const [mapUrl, setMapUrl] = useState('');
+  const [addressInput, setAddressInput] = useState('');
 
-  const getLocation = async (lat, lng) => {
+  const getMapUrl = async (lat, lng) => {
     const response = await fetch(
       `https://open.mapquestapi.com/staticmap/v5/map?key=${mapApiKey}&center=${lat},${lng}`
     );
-    console.log(response);
-    setMap(response);
+
+    return response.url;
   };
 
-  const getCoordinates = async () => {
+  const getCoordinates = async (location) => {
     const response = await fetch(
-      `http://www.mapquestapi.com/geocoding/v1/address?key=${mapApiKey}&location=1600+Pennsylvania+Ave+NW,Washington,DC,20500`
+      `http://www.mapquestapi.com/geocoding/v1/address?key=${mapApiKey}&location=${location}`
     );
+
     const coordinateData = await response.json();
-    return coordinateData.results[0].locations[0].latLng;
+    const result = coordinateData.results[0].locations[0].latLng;
+
+    return result;
   };
 
   // weaterAPI call
   const getWeather = async (lat, lng) => {
+    // const response = await fetch(
+    //   `http://api.weatherstack.com/current?access_key=abb76f98293860a41b391cd7208a22b5&query=New%20York`
+    // );
     const response = await fetch(
-      `http://api.weatherstack.com/current?access_key=${apiKey}&query=${lat},${lng}`
+      `http://api.weatherstack.com/current?access_key=${apiKey}&query=${encodeURIComponent(
+        lat
+      )},${encodeURIComponent(lng)}`
     );
-    const parsedResponse = await response.json();
-    console.log(response);
-    console.log(parsedResponse);
-    // setWeather(responseObject);
+    const weather = await response.json();
+
+    return weather;
   };
 
   // console.log(responseObject);
 
   const handleChange = (event) => {
-    console.log(event.target.value);
     setAddressInput(event.target.value);
   };
 
-  const handleSubmit = (event) => {
+  const handleSubmit = async (event) => {
     event.preventDefault();
-    const { lat, lng } = getCoordinates(addressInput);
-    getWeather(lat, lng);
-    getLocation();
+    const { lat, lng } = await getCoordinates(addressInput);
+    const weather = await getWeather(lat, lng);
+    console.log(weather);
+    setWeather(weather);
+    const mapUrl = await getMapUrl(lat, lng);
+    setMapUrl(mapUrl);
   };
 
   return (
@@ -106,9 +115,8 @@ function App() {
         </form>
       </header>
       <main>
-        <p>{apiKey}</p>
         <p>{weather?.current?.weather_descriptions}</p>
-        <img src={map.url} alt={`map of ${weather?.location?.region}`} />
+        <img src={mapUrl} alt={`map of ${weather?.location?.region}`} />
       </main>
     </div>
   );
