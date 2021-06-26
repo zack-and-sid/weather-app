01ed06c9f47b084989a4e8e96c6cdf9a2b6006b9
Sid Hayoun Lee <sidhlee@gmail.com>
2021-06-25 22:17:17 -0400

    Refactored using custom hooks and  api functions

--- src/App.js
+++ src/App.js
@@ -1,118 +1,26 @@
-import { useEffect, useState } from "react";
-import "./App.css";
-import useWeather from "./hooks/useWeather";
+import { useState } from 'react';
+import { getCoordinates } from './api';
+import './App.css';
+import useWeather from './hooks/useWeather';
+import useWeatherMap from './hooks/useWeatherMap';
 
 function App() {
-  const mapApiKey = process.env.REACT_APP_MAPQUEST_API;
-  const openWeatherAPI = process.env.REACT_APP_OPEN_WEATHER_API;
+  const [address, setAddress] = useState('');
 
-  const {
-    coordinates,
-    isLoading,
-    weather,
-    setAddress,
-    updateWeather,
-  } = useWeather();
-
-  // const [weather, setWeather] = useState({});
-  const [mapUrl, setMapUrl] = useState("");
-  const [addressInput, setAddressInput] = useState("");
-  // const [error, setError] = useState(null);
-
-  const getCoordinates = async (location) => {
-    const response = await fetch(
-      `http://www.mapquestapi.com/geocoding/v1/address?key=${mapApiKey}&location=${location}`
-    );
-    const coordinateData = await response.json();
-    const result = coordinateData.results[0].locations[0].latLng;
-
-    return result;
-  };
-
-  const getMapUrl = async ({ lat, lng }) => {
-    const response = await fetch(
-      `https://open.mapquestapi.com/staticmap/v5/map?key=${mapApiKey}&center=${lat},${lng}`
-    );
-
-    return response.url;
-  };
-
-  // weatherAPI call
-  const getWeather = async (lat, lng) => {
-    // const response = await fetch(
-    //   `http://api.weatherstack.com/current?access_key=abb76f98293860a41b391cd7208a22b5&query=New%20York`
-    // );
-    console.log({ lat, lng });
-    const url = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lng}&exclude=minutely&appid=${openWeatherAPI}&units=metric`;
-    console.log(openWeatherAPI);
-
-    try {
-      const response = await fetch(url);
-      const { current, daily, hourly } = await response.json();
-      console.log(response);
-
-      const weather = {
-        current,
-        daily: daily.map((day) => ({
-          temp: day.temp,
-          feelsLike: day.feels_like,
-          weather: day.weather[0],
-          moonPhase: day.moon_phase,
-          sunrise: day.sunrise,
-          sunset: day.sunset,
-          humidity: day.humidity,
-          clouds: day.clouds,
-          wind: day.wind_speed,
-          uvi: day.uvi,
-          pressure: day.pressure,
-        })),
-        hourly: hourly.map((hour) => {
-          const {
-            dt,
-            temp,
-            feels_like,
-            wind_deg,
-            wind_speed,
-            weather,
-            uvi,
-            pop,
-          } = hour;
-
-          return {
-            dt,
-            temp,
-            feels_like,
-            wind_deg,
-            wind_speed,
-            weather: weather[0],
-            uvi,
-            pop,
-          };
-        }),
-      };
-
-      return weather;
-    } catch (err) {
-      console.log(err);
-    }
-  };
-
-  // console.log(responseObject);
+  // Keeping weather and weatherMap in the state because they're connected to the element on the page
+  const { isWeatherLoading, updateWeather, weather } = useWeather();
+  const { weatherMap, updateWeatherMap } = useWeatherMap();
 
   const handleChange = (event) => {
-    setAddressInput(event.target.value);
+    setAddress(event.target.value);
   };
 
   const handleSubmit = async (event) => {
     event.preventDefault();
-    // const { lat, lng } = await getCoordinates(addressInput);
-    // const weather = await getWeather(lat, lng);
-    // console.log(weather);
-    // setWeather(weather);
-    // console.log(coordinates);
-    // const mapUrl = await getMapUrl(coordinates);
-    // setMapUrl(mapUrl);
-    updateWeather();
+    // not keeping coordinates in state because it's not directly connected to what gets rendered on the page
+    const coordinates = await getCoordinates(address);
+    updateWeather(coordinates);
+    updateWeatherMap(coordinates);
   };
 
   const description = weather?.current?.weather[0]?.description;
@@ -125,7 +33,7 @@ function App() {
           <label htmlFor="address">Address</label>
           <input
             type="text"
-            value={addressInput}
+            value={address}
             id="address"
             onChange={handleChange}
           />
@@ -133,8 +41,8 @@ function App() {
         </form>
       </header>
       <main>
-        {description && <p>{description}</p>}
-        <img src={mapUrl} alt={`map of ${weather?.location?.regio}`} />
+        <p>{isWeatherLoading ? 'loading...' : description}</p>
+        {weatherMap && <img src={weatherMap.url} alt={weatherMap.alt} />}
       </main>
     </div>
   );
--- src/hooks/useWeather.js
+++ src/hooks/useWeather.js
@@ -1,106 +1,24 @@
-import { useEffect, useState } from "react";
+import { useState } from 'react';
+import { getWeather } from '../api';
 
+/**
+ * @returns {{weather: Object, updateWeather: (coordinates) => void, isWeatherLoading: boolean}}
+ */
 export default function useWeather() {
   const [weather, setWeather] = useState();
-  const [isLoading, setIsLoading] = useState(false);
-  const [coordinates, setCoordinates] = useState();
-  const [address, setAddress] = useState("");
+  const [isWeatherLoading, setIsWeatherLoading] = useState(false);
 
-  useEffect(() => {
-    async function updateWeatherByAddress() {
-      const currentCoordinates = await getCoordinates(address);
-      setCoordinates(currentCoordinates);
-      const { lat, lng } = currentCoordinates;
-      const weather = await getWeather(lat, lng);
-      setWeather(weather);
-    }
-
-    if (isLoading) {
-      updateWeatherByAddress();
-      setIsLoading(false);
-    }
-  }, [isLoading]);
-
-  const updateWeather = () => {
-    setIsLoading(true);
+  const updateWeather = async (coordinates) => {
+    if (!coordinates) return;
+    setIsWeatherLoading(true);
+    const weather = await getWeather(coordinates);
+    setWeather(weather);
+    setIsWeatherLoading(false);
   };
 
   return {
     weather,
     updateWeather,
-    isLoading,
-    coordinates,
-    setAddress,
+    isWeatherLoading,
   };
 }
-
-const mapApiKey = process.env.REACT_APP_MAPQUEST_API;
-const openWeatherAPI = process.env.REACT_APP_OPEN_WEATHER_API;
-
-const getCoordinates = async (address) => {
-  const response = await fetch(
-    `http://www.mapquestapi.com/geocoding/v1/address?key=${mapApiKey}&location=${address}`
-  );
-
-  const coordinateData = await response.json();
-  const coordinates = coordinateData.results[0].locations[0].latLng;
-
-  return coordinates;
-};
-
-// weatherAPI call
-const getWeather = async (lat, lng) => {
-  console.log({ lat, lng });
-  const url = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lng}&exclude=minutely&appid=${openWeatherAPI}&units=metric`;
-  console.log(openWeatherAPI);
-
-  try {
-    const response = await fetch(url);
-    const { current, daily, hourly } = await response.json();
-    console.log(response);
-
-    const weather = {
-      current,
-      daily: daily.map((day) => ({
-        temp: day.temp,
-        feelsLike: day.feels_like,
-        weather: day.weather[0],
-        moonPhase: day.moon_phase,
-        sunrise: day.sunrise,
-        sunset: day.sunset,
-        humidity: day.humidity,
-        clouds: day.clouds,
-        wind: day.wind_speed,
-        uvi: day.uvi,
-        pressure: day.pressure,
-      })),
-      hourly: hourly.map((hour) => {
-        const {
-          dt,
-          temp,
-          feels_like,
-          wind_deg,
-          wind_speed,
-          weather,
-          uvi,
-          pop,
-        } = hour;
-
-        return {
-          dt,
-          temp,
-          feels_like,
-          wind_deg,
-          wind_speed,
-          weather: weather[0],
-          uvi,
-          pop,
-        };
-      }),
-    };
-
-    return weather;
-  } catch (err) {
-    console.log(err);
-  }
-};
